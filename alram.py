8abe94bab348ce4454a1df03cc5e9408ef7b14aa
ahpob <111692541+ahpob@users.noreply.github.com>
2022-09-07 19:01:40 +0900

    Update alram.py

--- alram.py
+++ alram.py
@@ -20,14 +20,14 @@ while True:
     try:
         res = requests.get(url, headers=headers)
         main_data1 = res.json()['summary']
-        initial = list(map(lambda x: (x['floorNo'],x['areaNo'],x['realSeatCntlk']),main_data1))
+        initial = list(map(lambda x: (x['floorNo'],x['areaNo'],x['realSeatCntlk'],x['seatGradeName']),main_data1))
         
 
         time.sleep(0.2)
 
         res = requests.get(url, headers=headers)
         main_data2 = res.json()['summary']
-        after = list(map(lambda x: (x['floorNo'],x['areaNo'],x['realSeatCntlk']),main_data2))
+        after = list(map(lambda x: (x['floorNo'],x['areaNo'],x['realSeatCntlk'],x['seatGradeName']),main_data2))
         
 
         for i in range(len(main_data2)):
