c81b8bd4a09516ae25afac5015f3a9571b5ee608
ahpob <111692541+ahpob@users.noreply.github.com>
2023-08-31 19:03:13 +0900

    Update alram.py

--- alram.py
+++ alram.py
@@ -9,7 +9,7 @@ from discord import SyncWebhook
 discord_url = os.environ['Discord_Url']
 webhook = SyncWebhook.from_url(discord_url)
 
-url = 'https://ticket.melon.com/reservation/popup/tktapi/product/block/summary.json?v=1&prodId=207434&pocCode=SC0002&scheduleNo=100001&seatGradeNo=&corpCodeNo='
+url = 'https://ticket.melon.com/reservation/popup/tktapi/product/block/summary.json?v=1&prodId=208654&pocCode=SC0002&scheduleNo=100001&seatGradeNo=&corpCodeNo='
 headers = {
             'Referer': 'ticket.melon.com',
             'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 Edg/104.0.1293.70'
