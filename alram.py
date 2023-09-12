e6d2d753df958b17feead05edd945c56829525fc
ahpob <111692541+ahpob@users.noreply.github.com>
2023-09-12 19:44:32 +0900

    Update alram.py

--- alram.py
+++ alram.py
@@ -1,38 +1,38 @@
-import requests,os
-import json,schedule,time
+import requests
+import os
+import json
+import time
 import datetime
 from discord import SyncWebhook
 
 discord_url = os.environ['Discord_Url']
 webhook = SyncWebhook.from_url(discord_url)
+
 current_time = datetime.datetime.now()
 adjusted_time = current_time + datetime.timedelta(hours=9)
 formatted_time = adjusted_time.strftime("%H:%M:%S")
+
 url = 'https://ticket.melon.com/reservation/popup/tktapi/product/block/summary.json?v=1&prodId=208654&pocCode=SC0002&scheduleNo=100001&seatGradeNo=&corpCodeNo='
+
 headers = {
-            'Referer': 'ticket.melon.com',
-            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 Edg/104.0.1293.70'
+    'Referer': 'ticket.melon.com',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 Edg/104.0.1293.70'
 }
 
-
 while True:
-    
     try:
         res = requests.get(url, headers=headers)
         main_data1 = res.json()['summary']
-        initial = list(map(lambda x: (x['seatGradeName'],x['areaNo'],x['lockSeatCntlk'],x['realSeatCntlk']),main_data1))
-        
-        
-            
+        initial = list(map(lambda x: (x['seatGradeName'], x['areaNo'], x['lockSeatCntlk'], x['realSeatCntlk']), main_data1))
+
         for i in range(len(main_data1)):
             lock = initial[i][2]
-            real = initial[i][3]         
- 
-            if lock >0 or real >0:
-                    
-             webhook.send(f"{formatted_time} - {initial[i]}")
-         time.sleep(0.3)          
+            real = initial[i][3]
 
-         
+            if lock > 0 or real > 0:
+                webhook.send(f"{formatted_time} - {initial[i]}")
+
+        time.sleep(0.4)
+
     except Exception as e:
-        print("error")                
+        print("error")
