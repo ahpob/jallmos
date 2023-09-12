1d338ea3acdad2e479626c5cbcbe2085db5db775
ahpob <111692541+ahpob@users.noreply.github.com>
2023-09-12 12:03:10 +0900

    Update alram.py

--- alram.py
+++ alram.py
@@ -9,8 +9,8 @@ current_time = datetime.datetime.now()
 adjusted_time = current_time + datetime.timedelta(hours=9)
 formatted_time = adjusted_time.strftime("%H:%M:%S")
                 
-url1 = 'https://ticket.melon.com/reservation/popup/tktapi/product/block/summary.json?v=1&prodId=207126&pocCode=SC0002&scheduleNo=100001&seatGradeNo=&corpCodeNo='
-url2 = 'https://ticket.melon.com/reservation/popup/tktapi/product/block/summary.json?v=1&prodId=207126&pocCode=SC0002&scheduleNo=100002&seatGradeNo=&corpCodeNo='
+url1 = 'https://ticket.melon.com/reservation/popup/tktapi/product/block/summary.json?v=1&prodId=208654&pocCode=SC0002&scheduleNo=100001&seatGradeNo=&corpCodeNo='
+url2 = 'https://ticket.melon.com/reservation/popup/tktapi/product/block/summary.json?v=1&prodId=208654&pocCode=SC0002&scheduleNo=100002&seatGradeNo=&corpCodeNo='
 headers = {
             'Referer': 'ticket.melon.com',
             'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 Edg/104.0.1293.70'
